0f5e5908b1cacb1485bc4b5ed0896493cff6a5d2
davidhuarina25@gmail.com <73082343tigre3>
2020-09-16 16:29:38 -0400

    cambios iframe y ajax

--- modulos/assets/functionsGeneralIFrame.js
+++ modulos/assets/functionsGeneralIFrame.js
@@ -1,9 +1,70 @@
 
 function cargarClasesFrame(){
    $(":text").addClass("form-control");
-   //$("select").addClass("form-control selectpicker");
+   $("select").addClass("select-css");
    $("table").addClass("table");
    $("table").addClass("table-sm");
+   $("table").addClass("table-success");
+   
+   $(":button").removeClass("boton");
+   $(":button").addClass("btn");
+   $(":button").addClass("btn-sm");
+   $(":button").addClass("btn-success");
+
+   $(":reset").removeClass("boton");
+   $(":reset").addClass("btn");
+   $(":reset").addClass("btn-sm");
+   $(":reset").addClass("btn-success");
+   
+   $("a").attr("target","cuerpo");
+
+   /*enlaces dentro de tabla*/
+   $("table tr td a").each( function () {
+      $(this).attr("title",$(this).html());
+      $(this).addClass("btn");
+      $(this).addClass("btn-sm");
+
+      switch($(this).html().toLowerCase().trim()){
+         case "detalle":
+           $(this).html("<i class='fas fa-fw fa-reorder'></i>");
+           $(this).addClass("btn-warning");
+           $(this).addClass("text-white");
+         break;
+         case "editar":
+           $(this).html("<i class='fas fa-fw fa-edit'></i>");
+           $(this).addClass("btn-primary");
+           $(this).addClass("text-white");
+         break;
+         case "copiar":
+           $(this).html("<i class='fas fa-fw fa-copy'></i>");
+           $(this).addClass("btn-warning");
+           $(this).addClass("text-white");
+         break;
+         case "anular":
+           $(this).html("<i class='fas fa-fw fa-trash'></i>");
+           $(this).addClass("btn-danger");
+           $(this).addClass("text-white");
+         break;
+         case "&lt;--anterior":
+           $(this).html("<i class='fas fa-fw fa-arrow-left'></i> ANTERIOR");
+           $(this).addClass("btn-info");
+           $(this).addClass("text-white");
+         break;
+         case "siguiente--&gt;":
+           $(this).html("SIGUIENTE <i class='fas fa-fw fa-arrow-right'></i>");
+           $(this).addClass("btn-info");
+           $(this).addClass("text-white");
+         break;
+         default:
+           $(this).addClass("btn-link");
+         break;
+      }       
+
+  });
+   $("table tr td a h3").each( function () {
+      $(this).replaceWith("<b>"+$(this).html()+"</b>");
+   });
+
    $('table .titulo_tabla td').each( function () {
    	$(this).replaceWith("<th>"+$(this).text()+"</th>");
   } );
